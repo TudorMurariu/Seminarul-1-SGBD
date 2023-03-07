9631eb2acaddec81f1e210d7d8428ab37469797c
Tudor <tudorcristianmurariu@yahoo.com>
2023-03-07 16:38:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Seminarul_1_SGBD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
